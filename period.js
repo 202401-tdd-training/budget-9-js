676571064d64d98c84e9c5a28db70c91fc1ba669
Joey Chen <jay.hatelove@gmail.com>
2024-01-06 21:20:37 +0800

    remove useless if block

--- period.js
+++ period.js
@@ -15,16 +15,6 @@ export class Period {
         let overlappingStart = this.startMoment.isAfter(budget.firstDay())
             ? this.startMoment
             : budget.firstDay();
-        if (this.startMoment.format('yyyyMM') === budget.yearMonth) {
-            // overlappingEnd = budget.lastDay();
-            // overlappingStart = this.startMoment;
-        } else if (this.endMoment.format('yyyyMM') === budget.yearMonth) {
-            // overlappingStart = budget.firstDay();
-            // overlappingEnd = this.endMoment;
-        } else {
-            // overlappingEnd = budget.lastDay();
-            // overlappingStart = budget.firstDay();
-        }
         return overlappingEnd.diff(overlappingStart, 'days') + 1;
     }
 }
