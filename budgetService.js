39c80d6ff1ededa723e4fc5498019f6f2e03d657
Joey Chen <jay.hatelove@gmail.com>
2024-01-06 20:28:43 +0800

    introduce variable: startMoment

--- budgetService.js
+++ budgetService.js
@@ -9,34 +9,34 @@ export class BudgetService {
             return 0;
         }
 
-        if (moment(start).isSame(end)) {
+        let startMoment = moment(start);
+        if (startMoment.isSame(end)) {
             const targetBudget = budgets.find(item => {
                 return moment(item.yearMonth).isSame(start, 'month');
             });
             if (!targetBudget) return 0;
-            const daysAmount = moment(start).daysInMonth();
+            const daysAmount = startMoment.daysInMonth();
             return targetBudget.amount / daysAmount;
         }
 
-        if (moment(start).isSame(end, 'month')) {
+        if (startMoment.isSame(end, 'month')) {
             const targetBudget = budgets.find(item => {
                 return moment(item.yearMonth).isSame(start, 'month');
             });
             if (!targetBudget) return 0;
-            const daysDiff = Math.abs(moment(start).diff(end, 'days')) + 1;
-            const daysAmount = moment(start).daysInMonth();
+            const daysDiff = Math.abs(startMoment.diff(end, 'days')) + 1;
+            const daysAmount = startMoment.daysInMonth();
             return targetBudget.amount * daysDiff / daysAmount;
         }
 
         const targetBudget = budgets.filter(item => {
             return moment(item.yearMonth).isSameOrAfter(start, 'month') && moment(item.yearMonth).isSameOrBefore(end, 'month');
-            ;
         });
 
         return targetBudget.reduce((sum, budget, i) => {
             if (i === 0) {
                 const daysAmount = moment(budget.yearMonth).daysInMonth();
-                const daysDiff = Math.abs(moment(start).diff(moment(budget.yearMonth).endOf('month'), 'days')) + 1;
+                const daysDiff = Math.abs(startMoment.diff(moment(budget.yearMonth).endOf('month'), 'days')) + 1;
 
                 return sum + (budget.amount * daysDiff / daysAmount);
             }
