1dbd26e06cc6089741aa1c17d13d8556cc98136b
Joey Chen <jay.hatelove@gmail.com>
2024-01-06 21:22:00 +0800

    use another period for firstDay and lastDay

--- period.js
+++ period.js
@@ -10,8 +10,8 @@ export class Period {
 
     overlappingDays(budget) {
         const another = new Period(budget.firstDay(), budget.lastDay());
-        let firstDay = budget.firstDay();
-        let lastDay = budget.lastDay();
+        let firstDay = another.startMoment;
+        let lastDay = another.endMoment;
         let overlappingEnd = this.endMoment.isBefore(lastDay)
             ? this.endMoment
             : lastDay;
