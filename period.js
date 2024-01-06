521001528bddbcf668750df95d316188a61fce90
Joey Chen <jay.hatelove@gmail.com>
2024-01-06 21:19:39 +0800

    decide overlappingEnd by period domain logic

--- period.js
+++ period.js
@@ -9,16 +9,18 @@ export class Period {
     }
 
     overlappingDays(budget) {
-        let overlappingEnd;
+        let overlappingEnd = this.endMoment.isBefore(budget.lastDay())
+            ? this.endMoment
+            : budget.lastDay();
         let overlappingStart;
         if (this.startMoment.format('yyyyMM') === budget.yearMonth) {
-            overlappingEnd = budget.lastDay();
+            // overlappingEnd = budget.lastDay();
             overlappingStart = this.startMoment;
         } else if (this.endMoment.format('yyyyMM') === budget.yearMonth) {
             overlappingStart = budget.firstDay();
-            overlappingEnd = this.endMoment;
+            // overlappingEnd = this.endMoment;
         } else {
-            overlappingEnd = budget.lastDay();
+            // overlappingEnd = budget.lastDay();
             overlappingStart = budget.firstDay();
         }
         return overlappingEnd.diff(overlappingStart, 'days') + 1;
